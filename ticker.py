5b16bc6135cee1552c59c0cc783a19ca85476290
snobu <foo@snobu.org>
2015-10-02 16:48:29 +0300

    Added SIGTERM cleanup. If Azure at full health just show a smiley

--- ticker.py
+++ ticker.py
@@ -4,10 +4,19 @@ import sys
 import os
 import feedparser
 import time
+import signal
 sys.path.append(os.path.abspath("lib/"))
 from UHScroll import *
 
 UH.brightness(0.10)
+
+def sigterm_handler(signal, frame):
+    print 'DEBUG: SIGTERM received. Cleaning up HAT..'
+    UH.clear()
+    sys.exit(0)
+
+# Cleanup HAT on SIGTERM (CTRL-C)
+signal.signal(signal.SIGTERM, sigterm_handler)
 
 def scroll(msg, color):
     unicorn_scroll(msg , color, 200,0.05)
@@ -22,14 +31,17 @@ def scroll(msg, color):
 while True:
     azurestatusRSS = "https://azure.microsoft.com/en-us/status/feed/"
     feed = feedparser.parse(azurestatusRSS)
-
-    scroll(feed['channel']['title'], 'cyan')
+    print 'DEBUG: Fetching and parsing RSS..'
 
     if len(feed['items']) > 0:
+        scroll(feed['channel']['title'], 'cyan')
+
         for item in feed['items']:
             scroll(item['title'], 'orange')
             scroll(item['description'], 'orange')
+
     else:
-        scroll('FULL HEALTH', 'green')
-        time.sleep(0.5)
+        #scroll('FULL HEALTH', 'green')
+        #time.sleep(0.5)
         show_letter(special_smilie, 'green', 400)
+        time.sleep(59)
